fa67d4d27d3928dec11309688d2d57049ea3e973
lar <systemsofttransportlaks@gmail.com>
2024-08-24 22:46:50 +0530

    Added requirements and finished the Complete feature

--- WebTodoApp.py
+++ WebTodoApp.py
@@ -17,8 +17,13 @@ st.title('My Todo App')
 # st.subheader('This is my Todo App')
 # st.write("This app increases your focus & productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo_item in enumerate(todos):
+    check_status = st.checkbox(todo_item, key=todo_item+str(index))
+    if check_status:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo_item+str(index)]
+        st.rerun()
 
 st.text_input(label='Todo:', label_visibility='hidden',
               placeholder = 'Enter here the new Todo:',
--- functions.py
+++ functions.py
@@ -15,13 +15,13 @@ def get_todos(filepath=FILEPATH):
 
 
 def write_todos(todos_arg, filepath=FILEPATH):
-    print('filePath',filepath)
-    print('todos:',todos_arg)
+    # print('filePath',filepath)
+    # print('todos:',todos_arg)
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
 
-print("In Functions.py __name__: ", __name__)
+# print("In Functions.py __name__: ", __name__)
 if __name__ == "__main__":
     print ("hello from function.py")
     print (get_todos())
